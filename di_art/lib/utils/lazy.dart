17afe082dea10ce1c79452864ade707175359209
andrii.halunets <andrii.halunets@eleks.com>
2019-08-07 18:50:16 +0300

    Migrate `error` folder to Dart, Write tests, Change project structure

---
+++ di_art/lib/utils/lazy.dart
@@ -0,0 +1,48 @@
+/*
+ *
+ *  * Copyright 2019 DiArt.
+ *  *
+ *  * Licensed under the Apache License, Version 2.0 (the "License");
+ *  * you may not use this file except in compliance with the License.
+ *  * You may obtain a copy of the License at
+ *  *
+ *  *      http://www.apache.org/licenses/LICENSE-2.0
+ *  *
+ *  * Unless required by applicable law or agreed to in writing, software
+ *  * distributed under the License is distributed on an "AS IS" BASIS,
+ *  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  * See the License for the specific language governing permissions and
+ *  * limitations under the License.
+ *
+ */
+/// USAGE
+///
+/// Wrap the variable in the global lazy function:
+/// String text = lazy<String>(() => "Text")();
+
+/// API
+
+Lazy<T> lazy<T>(T Function() function) => Lazy._lazy(function);
+
+abstract class Lazy<T> {
+  T call();
+  factory Lazy._lazy(T Function() function) => _LazyImpl<T>(function);
+}
+
+/// Private
+
+class _LazyImpl<T> implements Lazy<T>{
+
+  final Function _initializer;
+  T _value;
+
+  _LazyImpl(this._initializer);
+
+  T call()  {
+    if (_value == null) {
+      _value = _initializer != null ? _initializer() : null;
+    }
+    return _value;
+  }
+
+}
--- di_art/test/di_art_test.dart
+++ di_art/test/di_art_test.dart
@@ -1,13 +1,31 @@
+import 'package:di_art/error/errors.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:di_art/src/di_art.dart';
-
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  errorFolderTests();
+}
+
+errorFolderTests() {
+  String msg = "Message";
+  group("error folder", () {
+    exceptionTest<BadScopeInstanceException>(BadScopeInstanceException(msg), msg);
+    exceptionTest<DefinitionOverrideException>(DefinitionOverrideException(msg), msg);
+    exceptionTest<DiArtAppAlreadyStartedException>(DiArtAppAlreadyStartedException(msg), msg);
+    exceptionTest<InstanceCreationException>(InstanceCreationException(msg, Exception()), msg);
+    exceptionTest<MissingPropertyException>(MissingPropertyException(msg), msg);
+    exceptionTest<NoBeanDefFoundException>(NoBeanDefFoundException(msg), msg);
+    exceptionTest<NoParameterFoundException>(NoParameterFoundException(msg), msg);
+    exceptionTest<NoPropertyFileFoundException>(NoPropertyFileFoundException(msg), msg);
+    exceptionTest<NoScopeDefinitionFoundException>(NoScopeDefinitionFoundException(msg), msg);
+    exceptionTest<ScopeAlreadyCreatedException>(ScopeAlreadyCreatedException(msg), msg);
+    exceptionTest<ScopeAlreadyCreatedException>(ScopeAlreadyCreatedException(msg), msg);
+  });
+}
+
+exceptionTest<T extends Exception>(Exception exception, String msg) {
+  test('$T test', () {
+    expect(() => throw exception,
+        allOf([throwsException, throwsA(isInstanceOf<T>())]));
+    expect(exception.toString(), equals("$T: $msg"));
   });
 }
